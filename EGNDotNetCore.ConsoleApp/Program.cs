7f46e4a8f9c2af5ada4c5e307d59b7afa8f8c062
Eliezergaynine <elizerkaynaing251@gmail.com>
2024-04-12 01:13:51 +0630

    EGNDotNetCore

--- EGNDotNetCore.ConsoleApp/Program.cs
+++ EGNDotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EGNDotNetCore.ConsoleApp;
+using System.Data;
+using System.Data.SqlClient;
+//ctrl+ =suggestion
+
 Console.WriteLine("Hello, World!");
+AdoDotNet adoDotNet = new AdoDotNet();
+//adoDotNet.Read();
+adoDotNet.Create("Test title2", "Test author2", "Test content2");
+
