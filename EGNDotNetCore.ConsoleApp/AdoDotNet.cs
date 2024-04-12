55261f57df4f242c39714eb691be9535f2f93265
Eliezergaynine <elizerkaynaing251@gmail.com>
2024-04-12 11:47:00 +0630

    [Complete] Ado.Net CRUD

--- EGNDotNetCore.ConsoleApp/AdoDotNet.cs
+++ EGNDotNetCore.ConsoleApp/AdoDotNet.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection.Metadata.Ecma335;
 
 namespace EGNDotNetCore.ConsoleApp
 {
@@ -63,6 +64,61 @@ namespace EGNDotNetCore.ConsoleApp
             connection.Close();
             string message = result > 0 ? "saving successful" : "Saving failed";
             Console.WriteLine(message);
+        }
+        public void Update(int id,string title, string author,string content)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"UPDATE [dbo].[Tbl_blog]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+ WHERE BlogId =@BlogId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+            string message = result > 0 ? "Update Data successfully" : "Update Data failed";
+            Console.WriteLine(message);
+        }
+       
+        public void Delete(int id)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            Console.WriteLine("Connection open");
+            string query = @"delete from Tbl_blog where BlogId =@BlogId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+            Console.WriteLine("Connection closed");
+            string message = result > 0 ? "Delete data successfully" : "Delete data failed";
+            Console.WriteLine(message);
+        }
+        public void Edit(int id)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            string query = @"select * from Tbl_blog where BlogId =@BlogId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable tb = new DataTable();
+            sqlDataAdapter.Fill(tb);
+            connection.Close();
+            if(tb.Rows.Count == 0)
+            {
+                Console.WriteLine("No data found");
+                return;
+            }
+            DataRow dr = tb.Rows[0];
+            Console.WriteLine("Blog id=> " + dr["BlogId"]);
+            Console.WriteLine("Blog title=> " + dr["BlogTitle"]);
+            Console.WriteLine("Blog Author=> " + dr["BlogAuthor"]);
+            Console.WriteLine("Blog Content=> " + dr["BlogContent"]);
         }
     }
 }
--- EGNDotNetCore.ConsoleApp/Program.cs
+++ EGNDotNetCore.ConsoleApp/Program.cs
@@ -7,6 +7,10 @@ using System.Reflection;
 
 Console.WriteLine("Hello, World!");
 AdoDotNet adoDotNet = new AdoDotNet();
-//adoDotNet.Read();
 adoDotNet.Create("Test title2", "Test author2", "Test content2");
+adoDotNet.Read();
+adoDotNet.Update(3, "Test title3", "Test author3", "Test content3");
+adoDotNet.Delete(3);
+adoDotNet.Edit(2);
+//Console.ReadKey();
 
